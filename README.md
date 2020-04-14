157f43ec5517c2bfba0766f3b88f4ce885e71b4d
hero <hero_workspace@163.com>
2020-04-14 18:14:21 +0800

    add get local imports

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ida_plugins
+### test_ida_get_local_imports.py
+    提取导入函数中，程序自带的动态库导入函数，以 json文件 输出到 被测程序的目录下
---
+++ test_ida_get_local_imports.py
@@ -0,0 +1,138 @@
+#coding:utf-8
+
+
+# from idaapi import *
+import idaapi
+import idautils
+
+import json
+
+
+
+
+def write_line_to_file(file_path, line_data):
+    with open(file_path, 'w') as f:
+        f.write(line_data)
+        f.write('\n')
+
+
+
+
+def get_specify_import_modules_info(local_module_list):
+    
+    module_info_list = list()
+    
+
+    number_of_import_modules = idaapi.get_import_module_qty()
+    
+    
+    for i in xrange(0, number_of_import_modules):
+        print(i)
+        module_info = dict()
+        module_name = idaapi.get_import_module_name(i)
+        print(module_name)
+        
+        
+        
+        if module_name in local_module_list:
+            
+            print("local module: %s"  %(module_name))
+            module_info["index"] = i
+            module_info["name"] = module_name
+            print(module_info)
+            module_info_list.append(module_info)
+        
+        
+    print(module_info_list)
+    return module_info_list
+
+
+def get_import_funcs(import_module_index):
+    
+    import_funcs_info = []
+    
+    def imports_callback(ea, name, ord):
+        
+        
+        import_funcs_info.append(('0x%x' % (ea), name))
+        # import_funcs_info.append((str(hex(ea)), name))
+        
+        return True
+   
+
+    idaapi.enum_import_names(import_module_index, imports_callback)
+    
+    return import_funcs_info
+
+    
+
+# C:/test.exe --> test
+def get_pre_basename(file_path):
+    
+    file_basename = os.path.basename(file_path)
+    file_prename = os.path.splitext(file_basename)[0]
+        
+    return file_prename
+    
+
+def get_local_dll_path(root_dir):
+    root_dir_abs = os.path.abspath(root_dir)
+    if os.path.exists(root_dir_abs) == False:
+        raise RuntimeError('The path(%s) does not exist!')
+
+    dll_path_list = list()
+    for root, dirs, files in os.walk(root_dir_abs):
+        for file in files:
+            if '.dll' == file[-4:] :
+                dll_path_list.append(os.path.join(root, file).replace('\\','/'))
+
+    return dll_path_list
+
+
+
+
+def get_local_module_name_list(root_dir):
+    
+    dll_path_list =  get_local_dll_path(root_dir)
+    
+    module_name_list = list()
+    for dll_path in dll_path_list:
+        module_name = get_pre_basename(dll_path)
+        module_name_list.append(module_name)
+    
+    
+    return module_name_list
+    
+    
+ 
+def dump_dict_to_file(file_path, dict_data):
+    with open(file_path, 'w') as f:
+        json.dump(dict_data, f)
+
+
+def main():
+    root_dir = idautils.GetIdbDir()
+    
+    local_module_name_list = get_local_module_name_list(root_dir)
+    
+    local_import_module_info_list = get_specify_import_modules_info(local_module_name_list)
+    
+    import_module_funcs = dict()
+    for module_info in local_import_module_info_list:
+        funcs = get_import_funcs(module_info["index"])
+        import_module_funcs[module_info["name"]] = funcs
+    
+    
+    dump_dict_to_file(os.path.join(root_dir, 'imports.json'), import_module_funcs)
+    
+    
+    
+    
+        
+    
+if __name__ == '__main__':
+    # TODO 根据参数，决定递归遍历的目录
+    # TODO 研究能否在 ida 中 直接用颜色标识出 属于程序本身提供的dll（这部份不知道ida是否支持imports窗口）
+    main()
+    
+
